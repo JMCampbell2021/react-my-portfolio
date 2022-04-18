2e47b5ce2ba66e0251bb3c6706c70642e4418900
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-04-18 01:16:04 -0400

    updated portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Footer() {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by Joseph M. Campbell
-      <FontAwesomeIcon icon="fa-brands fa-github" /></div>
-      
+    <footer className="w-100 mt-auto bg-secondary p-4 justify-content-center">
+      <div>&copy;2022 by Joseph M. Campbell</div>
     </footer>
   )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import CivicProject2 from '../../assets/images/CivicProject2.png';
+import WeatherDash from '../../assets/images/Weather Dashboard.png';
+import SpellingApp from '../../assets/images/Spelling App.png';
 
 export default function Portfolio() {
   return (
     <div className='container-fluid mh-100 p-5'>
       <h1>Portfolio</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <div className='row'>
+      <div className='col'>
+      <a href="https://boiling-fortress-60978.herokuapp.com/login">  <img src={CivicProject2} className="img-fluid img-thumbnail" alt='Civic Project'/>
+      </a>
+      </div>
+      <div className='col'>
+      <a href="https://jmcampbell2021.github.io/weather-dashboard/"> <img src={WeatherDash} className="img-fluid img-thumbnail" alt='Civic Project' /> </a>
+      </div>
+      <div className='col'>
+      <a href='https://zacharycampanelli.github.io/SpellingPractice/'>
+        <img src={SpellingApp} className="img-fluid img-thumbnail" alt='Civic Project' />
+      </a>
+      </div>  
+
+      </div>
     </div>
   );
 }
