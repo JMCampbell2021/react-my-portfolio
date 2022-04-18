23d8ca1a97e0947a26631105012de7f237e5be4f
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-04-17 22:27:57 -0400

    render pages and updated navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,41 +2,45 @@ import React from 'react';
 
 function NavBar ({ currentPage, handlePageChange }) {
   return (
-    <nav className='navbar navbar-light bg-light'>
-      <div className='container-fluid'>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-      <li className="nav-item">
-        <a
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <div class="container-fluid">
+      <a className='navbar-brand' href='#'>Joseph M. Campbell</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+          <a
           href="#about"
           onClick={() => handlePageChange('About')}
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
+          >
           About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
+          </a>
+          </li>
+          <li class="nav-item">
+          <a
           href="#portfolio"
           onClick={() => handlePageChange('Portfolio')}
           className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
+          >
           Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
+          </a>
+          </li>
+          <li class="nav-item">
+          <a
           href="#contact"
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
+          >
           Contact
-        </a>
-      </li>
-    </ul>
+          </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    </nav>
-    
-    
+  </nav>
   );
 }
 
