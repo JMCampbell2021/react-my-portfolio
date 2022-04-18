81f47fcb3ff14a05791e18fbdfebc93bc9f13f67
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-04-17 22:10:32 -0400

    added about, contact, portfolio code

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function NavBar ({ currentPage, handlePageChange }) {
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <div className='container-fluid'>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+    </div>
+    </nav>
+    
+    
+  );
+}
+
+export default NavBar;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import About from './pages/About';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
